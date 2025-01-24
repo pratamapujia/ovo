e2666da855af7f9cf492608b84e7d34d09753d2c
shofwanhadif <shofwanhadif@gmail.com>
2025-01-24 23:11:41 +0700

    update : footer

--- resources/views/admin/layouts/index.blade.php
+++ resources/views/admin/layouts/index.blade.php
@@ -104,11 +104,11 @@
         <footer>
           <div class="footer clearfix mb-0 text-muted">
             <div class="float-start">
-              <p>2025 &copy; Mazer</p>
+              <p>{{ Date('Y') }} &copy; Mazer</p>
             </div>
             <div class="float-end">
               <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                by <a href="https://saugi.me">Saugi</a> Develop Apps by <a href="https://github.com/shofwanhadif">Shofwan</a></p>
+                by <a href="https://saugi.me">Saugi</a> Develop Apps by <a href="https://github.com/pratamapujia">Pratama Puji A.</a></p>
             </div>
           </div>
         </footer>
