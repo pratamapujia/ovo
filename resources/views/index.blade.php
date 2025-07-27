6e0d371e1077cb5083e5ce3c2d19bf7fe24dd9ab
pratamapujia <pujiistiani123@gmail.com>
2025-07-27 22:59:23 +0700

    fix : footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -152,7 +152,7 @@
               </div>
               <div class="float-end">
                 <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                  by <a href="https://ahmadsaugi.com">Saugi</a> Develop Apps by <a href="https://github.com/shofwanhadif">Shofwan</a></p>
+                  by <a href="https://github.com/shofwanhadif">Shofwan</a></p>
               </div>
             </div>
           </div>
